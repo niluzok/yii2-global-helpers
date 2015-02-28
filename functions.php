22c7d4d379f3d3229ef9d275b830bc24331da84b
niluzok <Pazz@cpz.ru>
2015-02-28 21:55:32 +0300

    add helper functions

---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace niluzok\yii2\global_helpers;
+
+use \yii\helpers\VarDumper;
+
+function d($var)
+{
+    VarDumper::dump($var, 10, true);
+}
+
+function ds($var)
+{
+    return VarDumper::dumpAsString($var, 10, true);
+}
+
+function dd($var)
+{
+    VarDumper::dump($var, 10, true);
+    Yii::$app->end();;
+}
+
+function e($var)
+{
+    echo VarDumper::export($var)."\n";
+}
+
+function es($var)
+{
+    return VarDumper::export($var);
+}
